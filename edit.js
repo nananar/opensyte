c139ae16fa073abc8112ef9019e9e48456e7922f
nananar <103318408+nananar@users.noreply.github.com>
2022-04-14 16:42:02 +0900

    Add files via upload

---
+++ edit.js
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>potitto</title>
+    <link rel="stylesheet" href="edit.css">
+  </head>
+  <body>
+    <div class="header">
+      <div class="header-logo">ポチッと</div>
+      <div class="header-list">
+        <ul>
+          <li>
+            <a href="top.html">打刻</a>
+            
+          </li>
+          <li>
+            <a href="data.html">データ</a>
+            
+          </li>
+          <li>
+            <a href="graph.html">グラフ</a>
+            
+          </li>
+          <li>
+            <a href="edit.html">編集</a>
+          </li>
+          <li>
+            <a href="setup.html">設定</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="copy-container">
+        <h1>編集</h1>
+        <h3 class="section-title">
+          
+
+
+        </h3>
+        <p>メールアドレス（必須）</p>
+        <input>
+        
+        <p>お問い合わせ内容（必須）</p>
+        <textarea></textarea>
+        
+        <p>※必須項目は必ずご入力ください</p>
+         <input class="contact-submit" type="submit" name="" id="" value="送信" />
+      </div>
+      
+    
+      
+      
+    </div>
+
+    <div class="footer">
+      <div class="footer-logo">ポチッと</div>
+      <div class="footer-list">
+        <ul>
+          <li>会社概要</li>
+          <li>採用</li>
+          <li>
+            <a href="inquiry.html">お問い合わせ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
